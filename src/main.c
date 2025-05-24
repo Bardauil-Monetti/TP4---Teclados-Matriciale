b6d55c8dfca5a2c16f12b095dd20f70473a91e62
Bardauil-Monetti <gbardauil548@gmail.com>
2025-05-23 22:59:11 -0300

    Trabajo Práctico Nº4 - Teclados Matriciales

---
+++ src/main.c
@@ -0,0 +1,11 @@
+#include "stm32f103xb.h"
+#include "TECLADO_MATRICIAL_H"
+int main(void) {
+
+    void teclado_matricial_init (1, 2, 3, 4, 5, 6, 7);
+      if (tecla!= 0) {
+        }
+        int teclado_matricial (1, 2, 3, 4, 5, 6, 7);
+
+return 0;
+}
