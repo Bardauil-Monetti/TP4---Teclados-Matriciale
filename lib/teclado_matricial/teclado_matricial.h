17e1b9d70786842bb08a97518750dec10be89f2c
Bardauil-Monetti <gbardauil548@gmail.com>
2025-05-30 21:41:15 -0300

    Update teclado_matricial.h

--- lib/teclado_matricial/teclado_matricial.h
+++ lib/teclado_matricial/teclado_matricial.h
@@ -2,5 +2,5 @@
 #define TECLADO_MATRICIAL_H
 #include <stm32f103xb.h>
 void teclado_matricial_init(int f1, int f2, int f3, int f4, int c1, int c2, int c3, int c4); 
-int teclado_matricial(int f1, int f2, int f3, int f4, int c1, int c2, int c3, int c4); 
+char teclado_matricial(int f1, int f2, int f3, int f4, int c1, int c2, int c3, int c4); 
 #endif
